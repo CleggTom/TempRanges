e6d7741e7992a217e8ece98d432a63fa9a66d336
Tom <t.clegg17@imperial.ac.uk>
2023-06-15 14:26:57 +0200

    update

--- Code/psw_simulations.jl
+++ Code/psw_simulations.jl
@@ -2,7 +2,6 @@ using Pkg
 using Revise
 
 Pkg.activate(".")
-Pkg.update("FoodWebs")
 
 using FoodWebs
 using LinearAlgebra
@@ -11,83 +10,99 @@ using JLD2
 
 fw = FoodWebs
 
-#1) GENERATE SPECIES POOL
-N_pool = 100000
-sp_vec = [fw.species(0.1) for i = 1:N_pool];
+function get_jacobian(com::fw.Community, K)
+    if com.N > 0
+        J = zeros(com.N, com.N)
+        λ_vals = Vector{Number}(undef, K)
+        λ_vecs = Matrix{Number}(undef, K , com.N)
+        s = fw.structural_parameters(com)
 
-#2) ASSEMBLE COMMUNITIES
-#set temperatures
+        for i = 1:K
+            e = fw.generalised_parameters(com, K)
+            fw.generalised_jacobian!(J, e[i])
+            λ = eigen!(J)
+
+            λ_vals[i] = λ.values[end]
+            λ_vecs[i,:] .= λ.vectors[end,:]
+        end
+
+        d = Dict(:vals => λ_vals, :vecs => λ_vecs)
+
+        return(d)
+    else
+        println(com)
+        return(0)
+    end
+end
+
+N_vec = [20]
+C_vec = [0.2]
+αd_vec = [0.75]
+
+N_rep = 10
+N_trials = 50
+
 N_T = 50
 t_vec = range(0,1,length = N_T)
 
-function generate_mc(sp_vec; verbose = false, vk = 100)
-    #generate metacommunty
-    mc = fw.stable_metacommunity(sp_vec, 20, t_vec, T_range = 0.1, R = 43.0,
-             psw_threshold = 0.8, max_draws = 2000, verbose = verbose, vk = vk)
+#get masses function
+get_M(com) = [com.R .^ n for n = com.n]
 
-    mc_prob = deepcopy(mc)
-    mc_random = deepcopy(mc)
-
-    fw.check_metacommunity(mc);
-    
-    return mc_prob,mc_random
-end
-
-get_M(com) = (com.R .^ [s.n for s = com.sp])
-
-#3) DISPERSAL
-# FoodWebs.K_dispersals!(mc1,10)
-N_dispersal = 2
-N_rep = 100
-N_trials = 20
-
-res = zeros(N_T, N_dispersal,N_rep,2,N_trials)
-params = Array{fw.GeneralisedParameters, 5}(undef, N_T, N_dispersal,N_rep,2,N_trials)
-bodysize = Array{Vector{Float64},4}(undef, N_T, N_dispersal,N_rep,2)
-
-println(Threads.nthreads())
-
-Threads.@threads for r = 1:N_rep
-    println("r: ", r, " Thread: ", Threads.threadid())
-    mc_prob,mc_random = generate_mc(sp_vec, verbose = false)
-    
-    
-    for d = 1:N_dispersal
-        #probabablistic
-        for (i,c) = enumerate(mc_prob.coms)
-            #itterate for given web structure
-            for trial = 1:N_trials
-                p = fw.generalised_parameters(c)
-                J = similar(p.A)
-                fw.generalised_jacobian!(J,p)
-                res[i,d,r,1,trial] = fw.max_real_eigval(J) 
-                params[i,d,r,1,trial] = deepcopy(p)
-            end
-        end
+for N = eachindex(N_vec)
+    for C = eachindex(C_vec)
+        for αd = eachindex(αd_vec)
         
-        #random
-        for (i,c) = enumerate(mc_random.coms)
-            #itterate for given web structure
-            for trial = 1:N_trials
-                p = fw.generalised_parameters(c)
-                J = similar(p.A)
-                fw.generalised_jacobian!(J,p)
-                res[i,d,r,2,trial] = fw.max_real_eigval(J) 
-                params[i,d,r,2,trial] = deepcopy(p)
+            psw = zeros(N_T,N_rep,2,2)
+            vecs = Array{Matrix{Number}, 4}(undef,N_T, N_rep, 2,2)
+            vals = Array{Vector{Number}, 4}(undef,N_T, N_rep, 2,2)
+            bodysize = Array{Vector{Float64},4}(undef,N_T, N_rep, 2,2)
+
+            println(Threads.nthreads())
+
+            Threads.@threads for r = 1:N_rep
+                    println("N: ",N, " C: ",C," r: ", r, " Thread: ", Threads.threadid())
+
+                    mc = fw.stable_metacommunity(N_vec[N], C_vec[C], t_vec, T_range = 0.2, R = 43.0, psw_threshold = 0.8, verbose = false,max_draws = 2000, N_trials = 50)
+
+                    mc_prob = deepcopy(mc)
+                    mc_random = deepcopy(mc)
+
+                    # get_jacobian(mc.coms, 1)
+
+                    psw[:,r,1,1] .= psw[:,r,2,1] .= fw.proportion_stable_webs(mc, N_trials = N_trials);
+                    bodysize[:,r,1,1] .= get_M.(mc_prob.coms)
+                    bodysize[:,r,2,1] .= get_M.(mc_random.coms)
+
+
+                    fw.multiple_dispersal!(mc_prob, p_dispersal = :r, d_dispersal = :p, αd = αd)
+                    fw.multiple_dispersal!(mc_random, p_dispersal = :r, d_dispersal = :r, αd = αd)
+
+                    N_T = 50
+                    for c = 1:(N_T-1) # dont consider last community..
+                        if mc_prob.coms[c].N > 0
+                            λ = get_jacobian(mc_prob.coms[c], N_trials)
+                            psw[c,r,1,2] = mean(real(λ[:vals]) .< 0)
+                            vecs[c,r,1,2] = λ[:vecs]
+                            vals[c,r,1,2] = λ[:vals]
+                        end
+                        
+                        if mc_random.coms[c].N > 0
+                            λ = get_jacobian(mc_random.coms[c], N_trials)
+                            # print(mean(real(λ[:vals]) .< 0))
+                            psw[c,r,2,2] = mean(real(λ[:vals]) .< 0)
+                            vecs[c,r,2,2] = λ[:vecs]
+                            vals[c,r,2,2] = λ[:vals]
+                        end
+                    end
+
+                    bodysize[:,r,1,2] .= get_M.(mc_prob.coms)
+                    bodysize[:,r,2,2] .= get_M.(mc_random.coms)
             end
-        end
-        
-        bodysize[:,d,r,1] .= get_M.(mc_prob.coms)
-        bodysize[:,d,r,2] .= get_M.(mc_random.coms)
-
-
-        fw.multiple_dispersal!(mc_prob, p_dispersal = :p, d_dispersal = :p)
-        fw.multiple_dispersal!(mc_random, p_dispersal = :r, d_dispersal = :r)
-
-        if d % 100 == 0
-            print(i)
+            
+            fn = join(["./data/psw_test/simulations_",N,"_",C,"_",αd,".jld2"])
+            save(fn, Dict("psw" => psw, "vecs" => vecs,"vals" => vals, "bodysize" => bodysize))
+       
         end
     end
 end
 
-save("./data/simulations.jld2", Dict("res" => res, "params" => params, "bodysize" => bodysize))
